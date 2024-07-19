b4954f258c4babf783d2f75b3c97521a15371549
Ayad-Ben-Saliem <ayad.bslm@gmail.com>
2024-07-19 11:27:13 +0200

    Test some server endpoints.

--- test/endpoint_test.dart
+++ test/endpoint_test.dart
@@ -1,50 +1,33 @@
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:souq_aljomaa/main.dart';
-import 'package:souq_aljomaa/models/model1.dart';
-import 'package:souq_aljomaa/storage/restful.dart';
+import 'package:souq_aljomaa/data_provider/restful.dart';
 
 void main() {
-  test('description', () async {
-    final model1 = Model1.fromJson(const {
-      'id': null,
-      'at': '2024-07-15T16:05:33.880397',
-      'scanner': null,
-      'locality': 'عرادة',
-      'witness': 'خالد محمد جرانة',
-      'responsible': 'عرادة',
-      'firstName': 'عياد',
-      'fatherName': 'مصطفى',
-      'grandfatherName': 'عياد',
-      'lastName': 'بن سليم',
-      'motherName': 'فتحية علي النعيري',
-      'identifierNo': '366166',
-      'nationalId': '119920025083',
-      'testimony': 'غير متزوج بغير ليبية',
-      'date': '2024-07-15T16:02:05.106218',
-    });
+  // test('save model test', () async {
+  //   final model1 = Model1.fromJson(const {
+  //     'id': null,
+  //     'at': '2024-07-15T16:05:33.880397',
+  //     'scanner': null,
+  //     'locality': 'عرادة',
+  //     'witness': 'خالد محمد جرانة',
+  //     'responsible': 'عرادة',
+  //     'firstName': 'عياد',
+  //     'fatherName': 'مصطفى',
+  //     'grandfatherName': 'عياد',
+  //     'lastName': 'بن سليم',
+  //     'motherName': 'فتحية علي النعيري',
+  //     'identifierNo': '366166',
+  //     'nationalId': '119920025083',
+  //     'testimony': 'غير متزوج بغير ليبية',
+  //     'date': '2024-07-15T16:02:05.106218',
+  //   });
+  //
+  //   Restful.initialize('http://localhost:5000');
+  //   final result = await Restful.saveModel(model1);
+  //   print(result);
+  // });
 
-    final result = await Restful.saveModel(model1);
-    print(result);
+  test('backup test', () async {
+    Restful.initialize('http://localhost:5000');
+    await Restful.backup(savePath: '/home/', password: 'A5355850d.');
   });
 }
-
-
-var x = '''
-INSERT INTO Model1 (
-  at,
-  scanner,
-  locality,
-  witness,
-  responsible,
-  firstName,
-  fatherName,
-  grandfatherName,
-  lastName,
-  motherName,
-  identifierNo,
-  nationalId,
-  testimony,
-  date
-) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-''';
